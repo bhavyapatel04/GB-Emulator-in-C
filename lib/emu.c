cacde8ab1d2024bea7bdc807aa1edfba3fba8cf1
bhavyapatel04 <bhavyapatel04@gmail.com>
2024-08-05 13:52:14 -0400

    Updated include files and started with core emu

---
+++ lib/emu.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "emu.h"
+#include "cart.h"
+#include "cpu.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+/* 
+1. Cartridge Rom
+2. CPU
+3. Address Bus
+4. PPU
+5. Timer
+*/
+
+static emu_context ctx;
+emu_context *emu_get_context() {
+    return &ctx;
+}
+void delay(u32 ms) {
+    SDL_Delay(ms);
+}
+
+int emu_run(int argc, char** argv) {
+    if (argc < 2) {
+        printf("Usage: emu <rom_file>\n");
+        return -1;
+    }
+    if(!cart_load(argv[1])) {
+        printf("Failed to load ROM FILE: %s\n", argv[1]);
+        return -2;
+    }
+    printf("Loaded ROM FILE: %s\n", argv[1]);
+    SDL_Init(SDL_INIT_VIDEO);
+    printf("SDL INIT\n");
+    TTF_Init();
+    printf("TTF INIT\n");
+
+    cpu_init();
+    ctx.running = true;
+    ctx.paused = false;
+    ctx.ticks = 0;
+}
