fc07504ba0524f867d36a05c9dde278bf74e7212
paul.kanyuch <pwk29@scarletmail.rutgers.edu>
2024-08-26 17:26:37 -0400

    bus and cart updated

--- lib/cart.c
+++ lib/cart.c
@@ -168,3 +168,15 @@ bool cart_load(char *cart) {
 
     return true;
 }
+
+u8 cart_read(u16 address) {
+    //for now just ROM ONLY type supported...
+
+    return ctx.rom_data[address];
+}
+
+void cart_write(u16 address, u8 value) {
+    //for now, ROM ONLY...
+
+    NO_IMPL
+}
